d6869ba06ded9f05799b23d6d27634cbc26f7939
J <jimfinoc@gmail.com>
2015-03-08 12:39:34 -0400

    a simple function program

--- moveRobot.py
+++ moveRobot.py
@@ -60,69 +60,108 @@ Motor2.run(Adafruit_MotorHAT.FORWARD)
 #Motor1.run(Adafruit_MotorHAT.RELEASE)
 
 
-Motor1.setSpeed(0)
-Motor2.setSpeed(0)
-Motor2.run(Adafruit_MotorHAT.FORWARD)
-Motor1.run(Adafruit_MotorHAT.FORWARD)
-Motor1.setSpeed(25)
-Motor2.setSpeed(25)
-time.sleep(2)
-Motor1.run(Adafruit_MotorHAT.RELEASE)
-Motor2.run(Adafruit_MotorHAT.RELEASE)
+#Motor1.setSpeed(0)
+#Motor2.setSpeed(0)
+#Motor2.run(Adafruit_MotorHAT.FORWARD)
+#Motor1.run(Adafruit_MotorHAT.FORWARD)
+#Motor1.setSpeed(25)
+#Motor2.setSpeed(25)
+#time.sleep(2)
 
-
-def moveForward():
-    Motor1.setSpeed(0)
-    Motor2.setSpeed(0)
-    Motor2.run(Adafruit_MotorHAT.FORWARD)
-    Motor1.run(Adafruit_MotorHAT.FORWARD)
-    Motor1.setSpeed(50)
-    Motor2.setSpeed(50)
-    time.sleep(2)
+def stop():
     Motor1.run(Adafruit_MotorHAT.RELEASE)
     Motor2.run(Adafruit_MotorHAT.RELEASE)
 
-def rotateLeft():
+
+def moveForward(timeSleeping=3):
+    Motor1.run(Adafruit_MotorHAT.RELEASE)
+    Motor2.run(Adafruit_MotorHAT.RELEASE)
+    Motor1.setSpeed(75)
+    Motor2.setSpeed(75)
+    Motor2.run(Adafruit_MotorHAT.FORWARD)
+    Motor1.run(Adafruit_MotorHAT.FORWARD)
+    time.sleep(timeSleeping)
+    Motor1.run(Adafruit_MotorHAT.RELEASE)
+    Motor2.run(Adafruit_MotorHAT.RELEASE)
+    Motor1.setSpeed(0)
+    Motor2.setSpeed(0)
+
+def rotateLeft(timeSleeping=2):
     Motor1.setSpeed(0)
     Motor2.setSpeed(0)
     Motor2.run(Adafruit_MotorHAT.BACKWARD)
     Motor1.run(Adafruit_MotorHAT.FORWARD)
     Motor1.setSpeed(50)
     Motor2.setSpeed(50)
-    time.sleep(2)
+    time.sleep(timeSleeping)
     Motor1.run(Adafruit_MotorHAT.RELEASE)
     Motor2.run(Adafruit_MotorHAT.RELEASE)
 
-def rotateRight():
+def rotateRight(timeSleeping=2):
     Motor1.setSpeed(0)
     Motor2.setSpeed(0)
     Motor2.run(Adafruit_MotorHAT.FORWARD)
     Motor1.run(Adafruit_MotorHAT.BACKWARD)
     Motor1.setSpeed(50)
     Motor2.setSpeed(50)
-    time.sleep(2)
+    time.sleep(timeSleeping)
     Motor1.run(Adafruit_MotorHAT.RELEASE)
     Motor2.run(Adafruit_MotorHAT.RELEASE)
 
-def turnRight():
-    Motor1.setSpeed(0)
-    Motor2.setSpeed(0)
-    Motor2.run(Adafruit_MotorHAT.FORWARD)
-    Motor1.run(Adafruit_MotorHAT.FORWARD)
-    Motor1.setSpeed(25)
-    Motor2.setSpeed(75)
-    time.sleep(2)
-    Motor1.run(Adafruit_MotorHAT.RELEASE)
-    Motor2.run(Adafruit_MotorHAT.RELEASE)
-
-def turnLeft():
+def turnRight(timeSleeping=2):
     Motor1.setSpeed(0)
     Motor2.setSpeed(0)
     Motor2.run(Adafruit_MotorHAT.FORWARD)
     Motor1.run(Adafruit_MotorHAT.FORWARD)
     Motor1.setSpeed(75)
-    Motor2.setSpeed(25)
-    time.sleep(2)
+    Motor2.setSpeed(150)
+    time.sleep(timeSleeping)
     Motor1.run(Adafruit_MotorHAT.RELEASE)
     Motor2.run(Adafruit_MotorHAT.RELEASE)
 
+def turnLeft(timeSleeping=1):
+    Motor1.setSpeed(0)
+    Motor2.setSpeed(0)
+    Motor2.run(Adafruit_MotorHAT.FORWARD)
+    Motor1.run(Adafruit_MotorHAT.FORWARD)
+    Motor1.setSpeed(150)
+    Motor2.setSpeed(75)
+    time.sleep(timeSleeping)
+    Motor1.run(Adafruit_MotorHAT.RELEASE)
+    Motor2.run(Adafruit_MotorHAT.RELEASE)
+
+turnLeft(1)
+
+
+#def program():
+#    moveForward()
+#    rotateRight()
+#    moveForward()
+#    rotateLeft()
+
+def looping(inputValue="h"):
+    while (inputValue!="q"):
+        print inputValue
+        if str(inputValue)=="h":
+            print "---------------------------------"
+            print "press the key and enter to "
+            print "h : to get this menu"
+            print ""
+            print "f : to go forward"
+            print "b : to go backward"
+            print ""
+            print "r : to rotate right"
+            print "t : to rotate left"
+            print ""
+            print "d : to turn left"
+            print "g : to turn right"
+            print ""
+            print "q : to quit"
+            print "---------------------------------"
+        if inputValue=="f":
+            moveForward()
+        inputValue = input("Enter your command: ")
+        print inputValue
+
+
+looping()
